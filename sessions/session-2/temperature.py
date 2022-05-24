ed1848c2aa9227c263d5c169e5e456d53e58e994
Pedro A. Mendez <pedroamendez421@gmail.com>
2022-05-24 14:21:43 -0400

    Added implementation of temperature API for session 2

--- sessions/session-2/temperature.py
+++ sessions/session-2/temperature.py
@@ -22,71 +22,126 @@ class Temperature():
         """
             Adds the current temperature with another temperature.
         """
-
-        # TODO: Check for scales.
-        pass
+        
+        primary_scale = self.Scale
+        other = other.ConvertToTarget(primary_scale)
+        result = round(self.Value, other.Value, 2)
+        return Temperature(result, primary_scale)
 
     def Substract(self, other: Temperature) -> Temperature:
         """
             Substracts the current temperature with another temperature.
         """
 
-        # TODO: Check for scales.
-        pass
+        primary_scale = self.Scale
+        other = other.ConvertToTarget(primary_scale)
+        result = round(self.Value - other.Value, 2)
+        return Temperature(result, primary_scale)
 
     def MultiplyBy(self, other: Temperature) -> Temperature:
         """
             Multiplies the current temperature with another temperature.
         """
 
-        # TODO: Check for scales.
-        pass
+        primary_scale = self.Scale
+        other = other.ConvertToTarget(primary_scale)
+        result = round(self.Value * other.Value, 2)
+        return Temperature(result, primary_scale)
 
     def DivideBy(self, other: Temperature) -> Temperature:
         """
             Divides the current temperature with another temperature.
         """
 
-        # TODO: Check for scales.
-        pass
+        primary_scale = self.Scale
+        other = other.ConvertToTarget(primary_scale)
+        result = round(self.Value / other.Value, 2)
+        return Temperature(result, primary_scale)
+        
 
     def ToFarenheit(self) -> Temperature:
         """
             Converts the current temperature to the farenheit scale.
         """
 
-        # TODO: Search for formula.
-        pass
+        result = None
+        scale = TemperatureScale.Farenheit
+        if (self.Scale.name == TemperatureScale.Celcius.name):
+            result = round((self.Value * 1.8) + 32, 2)
+            pass
+        
+        elif (self.Scale.name == TemperatureScale.Kelvin.name):
+            result = round((self.Value * 1.8) - 459.67, 2)
+            pass
+
+        return Temperature(result, scale)
 
     def ToCelcius(self) -> Temperature:
         """
             Converts the current temperature to the celcius scale.
         """
 
-        # TODO: Search for formula.
-        pass
+        result = None
+        scale = TemperatureScale.Celcius
+        if (self.Scale.name == TemperatureScale.Kelvin.name):
+            result = round(self.value - 273.15, 2)
+            pass
 
-    def ToKelvin() -> Temperature:
+        elif (self.Scale.name == TemperatureScale.Farenheit.name):
+            result = round((self.Value - 32) / 1.8, 2)
+            pass
+
+        return Temperature(result, scale)
+
+    def ToKelvin(self) -> Temperature:
         """
             Converts the current temperature to the kelvin scale.
         """
 
-        # TODO: Search for formula.
-        pass
+        result = None
+        scale = TemperatureScale.Kelvin
+        if (self.Scale.name == TemperatureScale.Celcius.name):
+            result = round(self.Value + 273.15, 2)
+            pass
 
-    def ToString() -> str:
+        elif (self.Scale.name == TemperatureScale.Farenheit.name):
+            result = round(((self.Value - 32) / 1.8) + 273.15, 2)
+            pass
+
+        return Temperature(result, scale)
+
+    def ToString(self) -> str:
         """
             Converts the current temperature to a string representation.
         """
 
-        # TODO: Implement this method.
-        pass
+        scale_char = self.Scale.name.capitalize()[0]
+        str_equivalent = f"{self.Value}{scale_char}"
+        return str_equivalent        
 
-    def ConvertToTarget() -> Temperature:
+    def ConvertToTarget(self, target_scale: TemperatureScale) -> Temperature:
         """
             Given a scale, converts the current temperature to the specified scale.
             This is used as the conversion wrapper.
         """
 
-        # TODO: Implement this method.
-        pass
+        if (self.Scale.name == target_scale.name):
+            return self
+
+        conversion = None
+        if (target_scale.name == TemperatureScale.Kelvin.name):
+            conversion = self.ToKelvin()
+            pass
+
+        elif (target_scale.name == TemperatureScale.Farenheit.name):
+            conversion = self.ToFarenheit()
+            pass
+
+        elif (target_scale.name == TemperatureScale.Celcius.name):
+            conversion = self.ToCelcius()
+            pass
+
+        else:
+            raise Exception("Invalid temperature scale.") # Exception
+
+        return conversion
